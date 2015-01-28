b8680289b3b293417d5767b0811df67fb7a2651c
Waqar-un-nisa Nabi <faintingdonkey@gmail.com>
2015-01-28 14:35:58 +0000

    Created new Database

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -15,6 +15,7 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
+    context_dict = { 'boldmessage': "I am bold too!"}
     return render(request, 'rango/about.html', context_dict)
 
 def category(request, category_name_slug):
